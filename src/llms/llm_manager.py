752a69fbd8ccc3af8fafd3372790bf7fcb96c89a
Alberto Daniel Rodriguez <arodri39@nd.edu>
2023-11-30 15:34:06 -0800

    Adding openAI envs

--- src/llms/llm_manager.py
+++ src/llms/llm_manager.py
@@ -1,10 +1,21 @@
+import os
 from typing import TypeVar
 
 import openai
+from dotenv import load_dotenv
 
 from llms.llm_models import OpenAIModel
 
 AIObject = TypeVar("AIObject")
+
+load_dotenv()
+OPEN_AI_ORG = os.environ["OPEN_AI_ORG"]
+OPEN_AI_KEY = os.environ["OPEN_AI_KEY"]
+
+assert OPEN_AI_ORG and OPEN_AI_KEY, f"Must supply value for {f'{OPEN_AI_ORG=}'.split('=')[0]} " \
+                                    f"and {f'{OPEN_AI_KEY=}'.split('=')[0]} in .env"
+openai.organization = OPEN_AI_ORG
+openai.api_key = OPEN_AI_KEY
 
 
 class LLMManager:
@@ -21,6 +32,7 @@ class LLMManager:
         :param model: The OpenAI model to use.
         :return: The response from open AI.
         """
+
         assert isinstance(model, OpenAIModel), f"Expected OpenAIModel to be passed in but got {model}."
         params = {
             "temperature": temperature,
